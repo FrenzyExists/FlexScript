336c3f56677fdcabb9916e068c2c029b4a54301e
Frenzy <frenzyexists@gmail.com>
2021-08-26 13:09:18 -0400

    Basic Folder Structure DONE

---
+++ src/main/java/flexscript/app/App.java
@@ -0,0 +1,13 @@
+package flexscript.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
